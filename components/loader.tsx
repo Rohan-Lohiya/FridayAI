cec5727ab035a96a53cc1d050f0ab5c94aa8c33a
Rohan-Lohiya <rohanhero182004@gmail.com>
2025-08-12 17:22:49 +0530

    bug fixes

--- components/loader.tsx
+++ components/loader.tsx
@@ -6,7 +6,7 @@ export const Loader = () => {
       <div className="w-10 h-10 relative animate-spin">
         <Image src="/logo.png" alt="logo" fill></Image>
       </div>
-      <p className="text-sm text-muted-foreground">Jarvis is thinking...</p>
+      <p className="text-sm text-muted-foreground">Friday is thinking...</p>
     </div>
   );
 };
